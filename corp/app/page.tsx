769812c2dc1f837ed7f15918afd2a90de3822d16
level237 <Bramslevel129@gmail.com>
2024-03-21 13:06:25 +0100

    Adding a Reusable Presentation Component

--- corp/app/page.tsx
+++ corp/app/page.tsx
@@ -1,26 +1,9 @@
 import Image from "next/image"
 import homeImg from "@/public/home.jpg";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
-      
-      Homepage
-      {/*<Image
-      src={homeImg}
-      alt="car factory"
-      fill
-      style={{ objectFit:"cover" }}
-  />*/}
-
-  <div className="flex flex-row">
-    <div className="absolute -z-10 inset-0">
-    <Image   src={homeImg} fill alt="car factory"/>
-    </div>
-   
-    <div>Buy our product</div>
-  </div>
-    </div>
-  
+    <Hero imgData={homeImg} imgAlt="car factory" title="Professional Cloud Hosting"/>
   );
 }
