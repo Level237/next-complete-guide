28c2a45497853b649a051da770576e36fa1ea73b
level237 <Bramslevel129@gmail.com>
2024-03-21 16:26:36 +0100

    Introducing Server Actions

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,8 +1,28 @@
-
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
 export default function SnippetCreatePage(){
+
+    async function createSnipped(formData:FormData){
+        //This needs to be a server action!
+        'use server'
+        //Check the user's input and make sure they're valid
+        const title=formData.get('title') as string;
+        const code=formData.get('code') as string;
+
+        //Create a new record in the database
+        const snipped=await db.snipped.create({
+            data:{
+                title,
+                code
+            }
+        })
+        console.log(snipped);
+        //Redirect the user back to the root route
+        redirect('/')
+    }
     return (
-        <form>
+        <form action={createSnipped}>
             <h3 className="font-bold m-3">Create a Snipped</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
