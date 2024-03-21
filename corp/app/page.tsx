732e2532509f5d9df9845406f3fea566de8d6472
level237 <Bramslevel129@gmail.com>
2024-03-21 12:39:07 +0100

    More on the Image Component

--- corp/app/page.tsx
+++ corp/app/page.tsx
@@ -6,12 +6,20 @@ export default function Home() {
     <div>
       
       Homepage
-      <Image
+      {/*<Image
       src={homeImg}
       alt="car factory"
       fill
       style={{ objectFit:"cover" }}
-      />
+  />*/}
+
+  <div className="flex flex-row">
+    <div className="absolute -z-10 inset-0">
+    <Image   src={homeImg} fill alt="car factory"/>
+    </div>
+   
+    <div>Buy our product</div>
+  </div>
     </div>
   
   );
