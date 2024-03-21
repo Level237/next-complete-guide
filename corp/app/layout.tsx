405e0744b07b9f1e9099cb62a2ae9aa43362cacf
level237 <Bramslevel129@gmail.com>
2024-03-21 11:58:03 +0100

    - Absolute Path Import Shortcut

--- corp/app/layout.tsx
+++ corp/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import Header from '../components/header';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ["latin"] });
 
