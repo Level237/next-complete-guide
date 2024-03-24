843331e87f85335479ba4fbb42d6d7cc7f021419
level237 <Bramslevel129@gmail.com>
2024-03-24 11:32:16 +0100

    More Caching Issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-'use server'
-import { Button } from "@nextui-org/react"
-import * as action from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-export default async function Home() {
 
-  const session=await auth();
+export default  function Home() {
 
   return (
     <div>
-      <form action={action.signIn}>
-      <Button type="submit">
-      Sign In
-    </Button>
-      </form>
-      <form action={action.signOut}>
-      <Button type="submit">
-      Sign Out
-    </Button>
-      </form>
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div>:<div>Signed Out</div>
-      }
-
-      <Profile/>
+      Home page
     </div>
    
   )
