3bb5aa1bd4689078821efc0e37e4fe618e5accfe
level237 <Bramslevel129@gmail.com>
2024-03-24 11:09:52 +0100

    Enabling Sign Out

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import * as actions from "@/actions"
 import {
     Navbar,
     NavbarBrand,
@@ -7,8 +7,12 @@ import {
     NavbarItem,
     Input,
     Button,
-    Avatar
+    Avatar,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
 } from '@nextui-org/react'
+
 
 import {auth} from '@/auth'
 
@@ -18,14 +22,29 @@ export default async function Header(){
     let authContent:React.ReactNode;
 
     if(session?.user){
-        authContent=<Avatar src={session.user.image || ''}/>
+        authContent=<Popover placement="left">
+            <PopoverTrigger>
+        <Avatar src={session.user.image || ''}/>
+        </PopoverTrigger>
+        <PopoverContent>
+            <div className="p-4">
+                <form action={actions.signOut}>
+                    <Button type="submit">Sign Out</Button>
+                </form>
+            </div>
+        </PopoverContent>
+        </Popover>
     }else{
         authContent=<>
         <NavbarItem>
+            <form action={actions.signIn}>
             <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+            </form>
         </NavbarItem>
         <NavbarItem>
+        <form action={actions.signIn}>
             <Button color="primary" variant="flat">Sign Up</Button>
+            </form>
         </NavbarItem>
         </>
         
