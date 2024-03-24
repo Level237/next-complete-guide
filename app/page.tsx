be3287fed9b9aeba8e802f9805d04ba82f3463b0
level237 <Bramslevel129@gmail.com>
2024-03-24 14:03:39 +0100

    Creating Topics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
 export default  function Home() {
 
   return (
-    <div>
-      Home page
+    <div className="grid grid-cols-4 gap p-4">
+     <div className="col-span-3">
+      <h1 className="text-xl m-2">Top Post</h1>
+     </div>
+     <div>
+    <TopicCreateForm/>
+     </div>
     </div>
    
   )
---
+++ components/topics/topic-create-form.tsx
@@ -0,0 +1,6 @@
+
+export default function TopicCreateForm(){
+    return (
+        <div>Topic create form</div>
+    )
+}
