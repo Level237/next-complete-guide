d7c0d5d65e7d6f523a1c754b32931419a0da4263
level237 <Bramslevel129@gmail.com>
2024-03-25 20:54:14 +0100

    Scaffolding the Topic Show Page

--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,4 +1,18 @@
+interface TopicShowPageProps{
+    params:{
+        slug:string;
+    }
+}
+export default function TopicShowPage({params}:TopicShowPageProps){
+    const {slug}=params
+    return <div className="grid grid-cols-4 gap-4 p-4">
+        <div className="col-span-3">
+    <h1 className="text-2xl font-bold mb-2">
+        {slug}
+    </h1>
+</div>
+<div>
 
-export default function TopicShowPage(){
-    return <div>Topic show</div>
+</div>
+</div>
 }
