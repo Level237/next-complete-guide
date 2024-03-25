5e6d2b561b80b04a61763b853379ee6466cd0eb8
level237 <Bramslevel129@gmail.com>
2024-03-25 08:52:49 +0100

    Handling General Form Errors

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import {z} from "zod"
-
+import { auth } from "@/auth";
 const createTopicSchema=z.object({
     name:z.string()
     .min(3)
@@ -14,7 +14,8 @@ const createTopicSchema=z.object({
 interface CreateTopicFormState{
     errors:{
         name?:string[];
-        description?:string[]
+        description?:string[];
+        _form?:string[],
     }
 }
 export async function createTopic(
@@ -25,6 +26,7 @@ export async function createTopic(
     const result=createTopicSchema.safeParse({
         name:formData.get('name'),
         description:formData.get('description'),
+        
     })
 
     if(!result.success){
@@ -32,9 +34,17 @@ export async function createTopic(
             errors:result.error.flatten().fieldErrors
            }
     }
+    const session=await auth();
+
+    if(!session || session.user ){
+        return{
+            errors:{
+                _form:['You must be signed in to do this.']
+            }
+        }
+    }
    return {
     errors:{}
    }
     // TODO:revalidate the homepage
-   
 }
