c416527bd6029ba8bd7f16032afed9fc74259282
level237 <Bramslevel129@gmail.com>
2024-03-28 19:29:53 +0100

    Define in Parent Fetch in Child

--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/posts/post-create-form";
+import PostList from "@/components/posts/post-list";
+import { fetchPostsByTopicSlug } from '../../../db/queries/posts';
 
 interface TopicShowPageProps{
     params:{
@@ -11,7 +13,9 @@ export default function TopicShowPage({params}:TopicShowPageProps){
         <div className="col-span-3">
     <h1 className="text-2xl font-bold mb-2">
         {slug}
+       
     </h1>
+    <PostList fetchData={()=>fetchPostsByTopicSlug(slug)}/>
 </div>
 <div>
 <PostCreateForm slug={slug}/>
