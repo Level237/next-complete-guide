8d79b278aa877a9828a46c8c16e8f90f63167446
level237 <Bramslevel129@gmail.com>
2024-03-28 20:05:46 +0100

    - Alternative Type Names and Query Definitions

--- db/queries/posts.ts
+++ db/queries/posts.ts
@@ -1,15 +1,17 @@
 import type { Post } from "@prisma/client";
 import {db} from "@/db"
 
-export type PostWithData=(
-    Post & {
-        topic:{slug:string};
-        user:{name:string | null};
-        _count:{comments:number}
-    }
-)
+//export type PostWithData=(
+    //Post & {
+        //topic:{slug:string};
+        //user:{name:string | null};
+        //count:{comments:number}
+   // }
+//)
 
-export function fetchPostsByTopicSlug(slug:string):Promise<PostWithData[]>{
+export type PostWithData=Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number]
+
+export function fetchPostsByTopicSlug(slug:string){
         return db.post.findMany({
             where:{topic:{slug}},
             include:{
