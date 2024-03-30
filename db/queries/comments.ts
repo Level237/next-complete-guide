8f66b419ee3126703db5b60e98eee854cb78e2bd
level237 <Bramslevel129@gmail.com>
2024-03-30 09:14:34 +0100

    Deduplicating Requests with Cache

--- db/queries/comments.ts
+++ db/queries/comments.ts
@@ -1,12 +1,13 @@
 import type { Comment } from "@prisma/client";
 import { db } from "..";
+import { cache } from "react";
 
 export type CommentWithAuthor=
     Comment & {
         user:{name:string |null;image:string |null}
     }
 
-export function fetchCommentsByPostId(postId:string):Promise<CommentWithAuthor[]>{
+export const fetchCommentsByPostId=cache((postId:string):Promise<CommentWithAuthor[]>=>{
 
     console.log('Making a query');
     return db.comment.findMany({
@@ -21,4 +22,4 @@ export function fetchCommentsByPostId(postId:string):Promise<CommentWithAuthor[]
             }
         }
     })
-}
+})
