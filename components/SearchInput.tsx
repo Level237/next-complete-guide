230d2e51dfe1b01f3f41e05a9795148c2be8f8d3
level237 <Bramslevel129@gmail.com>
2024-04-05 10:39:29 +0100

    Accessing the Query String

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { Input } from '@nextui-org/react'
+import { useSearchParams } from 'next/navigation'
 
 export default function SearchInput() {
+  const searchParams=useSearchParams()
   return (
     <div>
-      <Input/>
+      <Input defaultValue={searchParams.get('term') || ""}/>
     </div>
   )
 }
